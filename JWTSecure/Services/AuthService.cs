6dbaa675649d026efee2f08f295259bb2ebba408
Eel2000 <eliezerbwana@gmaill.com>
2020-11-19 11:27:15 +0100

    🚧Add AuthService Class.

---
+++ JWTSecure/Services/AuthService.cs
@@ -0,0 +1,18 @@
+﻿using JWTSecure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTSecure.Services
+{
+    public class AuthService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
