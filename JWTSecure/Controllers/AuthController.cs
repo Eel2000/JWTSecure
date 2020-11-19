67518275b0e5ea3df2b8a3f63aea5d0812db4701
Eel2000 <eliezerbwana@gmaill.com>
2020-11-19 14:50:09 +0100

    🚧Configure AuthController For MainLunching

--- JWTSecure/Controllers/AuthController.cs
+++ JWTSecure/Controllers/AuthController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace JWTSecure.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     [AllowAnonymous]
     public class AuthController : ControllerBase
