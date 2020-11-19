6b0847e8a7096c8637e8fc01f10e8f3786a5cc9f
Eel2000 <eliezerbwana@gmaill.com>
2020-11-19 11:42:46 +0100

    📦Add JWT Lib

--- JWTSecure/Services/AuthService.cs
+++ JWTSecure/Services/AuthService.cs
@@ -1,4 +1,5 @@
 ﻿using JWTSecure.Context;
+using JWTSecure.Services.interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,13 +7,28 @@ using System.Threading.Tasks;
 
 namespace JWTSecure.Services
 {
-    public class AuthService
+    public class AuthService : IAuthService
     {
         private readonly ApplicationDbContext _context;
 
         public AuthService(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        public Task Login(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> Register(string usernane, string email, string password)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- JWTSecure/Services/interfaces/IAuthService.cs
+++ JWTSecure/Services/interfaces/IAuthService.cs
@@ -4,6 +4,7 @@ namespace JWTSecure.Services.interfaces
 {
     public interface IAuthService
     {
+        Task<bool> Logout();
         Task Login(string email, string password);
         Task<string> Register(string usernane, string email, string password);
     }
