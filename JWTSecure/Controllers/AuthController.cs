c1e68f856dd7ac7af3e2a385f355072212ec2292
Eel2000 <eliezerbwana@gmaill.com>
2020-11-19 14:44:41 +0100

    🚧Add Register And Login Actions

--- JWTSecure/Controllers/AuthController.cs
+++ JWTSecure/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JWTSecure.Models;
+using JWTSecure.Services.interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,7 +12,45 @@ namespace JWTSecure.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [AllowAnonymous]
     public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        //Post: api/Auth/Register
+        [HttpPost("Register")]
+        public async Task<IActionResult> Account([FromBody] AppUser user)
+        {
+            try
+            {
+                var result = await _authService.Register(user.UserName, user.EmailAdress, user.Password);
+
+                return Ok($"User Created With password! => {result}");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Faild to create User with Password =>" + e.Message);
+            }
+        }
+
+        [HttpGet("login")]
+        public async Task<IActionResult> Login(string email,string password)
+        {
+            try
+            {
+                var result = await _authService.Login(email, password);
+
+                return Ok($"User Log In Successfully => {result}");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Fail to Login => {e.Message}");
+            }
+        }
     }
 }
