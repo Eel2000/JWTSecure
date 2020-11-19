90c23f3df1edd1dd8691308f27b081b4d996ef15
Eel2000 <eliezerbwana@gmaill.com>
2020-11-19 11:30:26 +0100

    🚧Add IAuthService Interface.

---
+++ JWTSecure/Services/interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace JWTSecure.Services.interfaces
+{
+    public interface IAuthService
+    {
+        Task Login(string email, string password);
+        Task<string> Register(string usernane, string email, string password);
+    }
+}
