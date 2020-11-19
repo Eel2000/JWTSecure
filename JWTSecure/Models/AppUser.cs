8a05ba24cc8fe6c05ca130b56fbb608ab7d63128
Eel2000 <eliezerbwana@gmaill.com>
2020-11-19 10:00:20 +0100

    ✨Add  User Model

---
+++ JWTSecure/Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTSecure.Models
+{
+    public class AppUser
+    {
+        public string ID { get; set; }
+        public string UserName { get; set; }
+        public string EmailAdress { get; set; }
+        public string Password { get; set; }
+    }
+}
