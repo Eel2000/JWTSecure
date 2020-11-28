158d4a9ca4827d5a5a99016af7591194c34c01ba
Eel2000 <eliezerbwana@gmaill.com>
2020-11-28 12:17:33 +0100

    ✨Add Xamarin.Forms Project To Solution To Consumme APIs

---
+++ AuthWithJwt/AuthWithJwtModule.cs
@@ -0,0 +1,20 @@
+﻿using AuthWithJwt.ViewModels;
+using AuthWithJwt.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace AuthWithJwt
+{
+    public class AuthWithJwtModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ViewA, ViewAViewModel>();
+        }
+    }
+}
---
+++ JWTSecure/Migrations/JWTDb/20201123110821_scaffoldIdentityTb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JWTSecure.Migrations.JWTDb
+{
+    public partial class scaffoldIdentityTb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
