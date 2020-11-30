d938693a2b4b1734fae87360c2fd5d9121b420b3
Eel2000 <eliezerbwana@gmaill.com>
2020-11-30 09:37:29 +0100

    🚧Add Code Behind For SignIn And SignUp Command

--- AuthWithJwt/AuthWithJwt/ViewModels/MainPageViewModel.cs
+++ AuthWithJwt/AuthWithJwt/ViewModels/MainPageViewModel.cs
@@ -5,15 +5,55 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AuthWithJwt.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        private readonly INavigationService _navigationService;
+
+        private const string API_URL = "";
+
+        private string _userName;
+        private string _password;
+
+        public string UserName
+        {
+            get => _userName;
+            set => SetProperty(ref _userName, value);
+        }
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+
+        public ICommand SignInCommand { get; set; }
+        public ICommand SignUpCommand { get; set; }
+
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             Title = "Main Page";
+
+            _navigationService = navigationService;
+        }
+
+        private async Task SignIn()
+        {
+            if (string.IsNullOrWhiteSpace(UserName) || string.IsNullOrWhiteSpace(Password))
+                await App.Current.MainPage.DisplayAlert("Wraning", "Some Fields Still Empty , please Fill them before your sbmit", "got it");
+
+            //call sign in API here
+        }
+
+        private async Task SignUp()
+        {
+            //got to register page
+            await _navigationService.NavigateAsync("");
         }
     }
 }
